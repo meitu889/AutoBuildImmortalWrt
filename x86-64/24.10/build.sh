1e0da1864164d4c4a08ca18304ff714ae2dcb96a
meitu889 <meitu889@gmail.com>
2025-01-01 21:15:09 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -25,7 +25,7 @@ PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
 #20250101增加的24.10的软件,切记不要带版本号和_all，只安装语言ipk,#是无法编译的，跳过。
 #PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn.ipk"
-PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn.ipk"
+#PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn.ipk"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn.ipk"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn.ipk"
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn.ipk"

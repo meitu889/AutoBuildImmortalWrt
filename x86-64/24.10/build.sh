8c7809261f2bdc25250d1503dcd1cc958044e13b
meitu889 <meitu889@gmail.com>
2025-01-01 21:52:37 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -23,7 +23,7 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
-#20250101增加的24.10的软件,切记不要带版本号和_all，只安装语言ipk,netdata、mwan3不能编译。
+#20250101增加的24.10的软件,切记不要带版本号和_all，只写名字就行。
 PACKAGES="$PACKAGES luci-i18n-netdata-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-mwan3-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
